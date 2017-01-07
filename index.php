4ce81d0ebd913ff01cae30a449a1cc0839023292
sandeepkamara <sndsndp8@gmail.com>
2017-01-07 16:35:19 +0530

    Create Setup

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="css/bootstrap.css" />
+	<link rel="stylesheet" href="css/bootstrap-grid.css" />
+	<link rel="stylesheet" href="css/bootstrap-reboot.css" />
+	<link rel="stylesheet" href="css/custom.css" />
+	<title>My Blog</title>
+</head>
+<body>
+	
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/custom.js"></script>
+</body>
+</html>
