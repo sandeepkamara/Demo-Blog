c504a9c41fa079b7400c7090d7451af4e4a3cc5e
sandeepkamara <sndsndp8@gmail.com>
2017-01-07 19:09:57 +0530

    changes by kamara

--- index.php
+++ index.php
@@ -2,15 +2,38 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8" />
-	<link rel="stylesheet" href="css/bootstrap.css" />
-	<link rel="stylesheet" href="css/bootstrap-grid.css" />
-	<link rel="stylesheet" href="css/bootstrap-reboot.css" />
-	<link rel="stylesheet" href="css/custom.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, initial-scale=1, maximum-scale=1" />
 	<title>My Blog</title>
+	<link type="text/css" rel="stylesheet" href="css/tether.css" />
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.css" />
+	<link type="text/css" rel="stylesheet" href="css/bootstrap-grid.css" />
+	<link type="text/css" rel="stylesheet" href="css/bootstrap-reboot.css" />
+	<link type="text/css" rel="stylesheet" href="css/custom.css" />
+	<link type="text/css" rel="stylesheet" href="css/font-awesome.min.css" />
 </head>
 <body>
-	
+	<section id="banner-slider">
+		<div id="carouselExampleControls" class="carousel carousel-fade" data-ride="carousel">
+			<div class="carousel-inner" role="listbox">
+				<div class="carousel-item active">
+					<img class="d-block img-fluid" src="images/slider1.jpg" alt="First slide" />
+				</div>
+				<div class="carousel-item">
+					<img class="d-block img-fluid" src="images/slider2.jpg" alt="Second slide" />
+				</div>
+			</div>
+			<a class="carousel-control-prev slider-control" href="#carouselExampleControls" role="button" data-slide="prev">
+				<span class="fa fa-angle-left banner-slide-icons" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next slider-control" href="#carouselExampleControls" role="button" data-slide="next">
+				<span class="fa fa-angle-right banner-slide-icons" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>		
+	</section>
 	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/tether.js"></script>
 	<script type="text/javascript" src="js/bootstrap.js"></script>
 	<script type="text/javascript" src="js/custom.js"></script>
 </body>
